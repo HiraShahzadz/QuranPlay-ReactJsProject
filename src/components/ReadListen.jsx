b01a898f7336d1aef77497f0b84d230c53a40aee
Hira Shahzad <hirashahzad565@gmail.com>
2024-03-31 22:46:18 +0500

    Add Read listen page and show ayat and audios

---
+++ src/components/ReadListen.jsx
@@ -0,0 +1,13 @@
+import Translation from "./Translation";
+
+function ReadListen() {
+  return (
+    <div>
+      <div className="md:ml-44 xl:ml-48 md:mr-44 xl:mr-48">
+        <Translation />
+      </div>
+    </div>
+  );
+}
+
+export default ReadListen;
