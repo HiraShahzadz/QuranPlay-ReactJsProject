7fa9698eea2229a2267b4a79388b618184d937e5
Hira Shahzad <hirashahzad565@gmail.com>
2024-03-31 22:44:00 +0500

    Add Blogs and designed it

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,295 @@
+function Blogs() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="m-5 mb-9">
+        <div role="main" className="flex flex-col items-center justify-center">
+          <h2 className="text-center text-green-600 text-xl md:sm:text-3xl font-bold ">
+            المدونات
+          </h2>
+          <h1 className="text-center mt-5 text-black text-xl md:sm:text-3xl font-bold mb-2">
+            Blogs
+          </h1>
+          <p className="text-center text-black">
+            Explore your favourite topics
+          </p>
+        </div>
+        <div className="lg:flex items-stretch md:mt-12 mt-8">
+          <div className="lg:w-1/2">
+            <div className="sm:flex items-center justify-between xl:gap-x-8 gap-x-6">
+              <div className="sm:w-1/2 relative ">
+                <div>
+                  <p className="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
+                    March 29, 2023
+                  </p>
+                  <div className="absolute bottom-0 left-0 p-6">
+                    <h2 className="text-xl font-semibold 5 text-white">
+                      When is Ramadan?
+                    </h2>
+                    <p className="text-base leading-4 text-white mt-2">
+                      Ramadan is the holy
+                    </p>
+                    <a
+                      href="https://blog.quranmajeed.com/when-is-ramadan/)"
+                      className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline"
+                    >
+                      <p className="pr-2 text-sm font-medium leading-none">
+                        Read More
+                      </p>
+                      <svg
+                        className="fill-stroke"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M5.75 12.5L10.25 8L5.75 3.5"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <img
+                  src="https://hips.hearstapps.com/hmg-prod/images/lantern-that-have-moon-symbol-on-top-and-small-royalty-free-image-1647454764.jpg?crop=0.665xw:1.00xh;0.337xw,0&resize=980:*"
+                  className="w-full object-cover rounded-lg"
+                  alt="chair"
+                />
+              </div>
+              <div className="sm:w-1/2 sm:mt-0 mt-4 relative">
+                <div>
+                  <p className="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
+                    April 30, 2023
+                  </p>
+                  <div className="absolute bottom-0 left-0 p-6">
+                    <h2 className="text-xl font-semibold 5 text-white">
+                      What does Halal Mean?
+                    </h2>
+                    <p className="text-base leading-4 text-white mt-2">
+                      The Arabic word Halal is
+                    </p>
+                    <a
+                      href="https://blog.quranmajeed.com/what-does-halal-mean/)"
+                      className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline"
+                    >
+                      <p className="pr-2 text-sm font-medium leading-none">
+                        Read More
+                      </p>
+                      <svg
+                        className="fill-stroke"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M5.75 12.5L10.25 8L5.75 3.5"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <img
+                  src="https://img.freepik.com/premium-photo/black-green-rough-grainy-gradient-background_135595-69545.jpg?w=1380"
+                  className="w-full rounded-lg"
+                  alt="wall design "
+                />
+              </div>
+            </div>
+            <div className="relative">
+              <div>
+                <p className="md:p-10 p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
+                  March 30, 2023
+                </p>
+                <div className="absolute bottom-0 left-0 md:p-10 p-6">
+                  <h2 className="text-xl font-semibold 5 text-white">
+                    What is Eid Al Fitr?
+                  </h2>
+                  <p className="text-base leading-4 text-white mt-2">
+                    Eid Al Fitr is an
+                  </p>
+                  <a
+                    href="https://blog.quranmajeed.com/what-is-eid-al-fitr/"
+                    className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline"
+                  >
+                    <p className="pr-2 text-sm font-medium leading-none">
+                      Read More
+                    </p>
+                    <svg
+                      className="fill-stroke"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.75 12.5L10.25 8L5.75 3.5"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <img
+                src="https://img.freepik.com/premium-vector/eid-al-fitr-celebration-line-art_8589-244.jpg?w=1380"
+                alt="sitting place"
+                className="w-full mt-8 md:mt-6 hidden sm:block rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="lg:w-1/2 xl:ml-8 lg:ml-4 lg:mt-0 md:mt-6 mt-4 lg:flex flex-col justify-between">
+            <div className="relative">
+              <div>
+                <p className="md:p-10 p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
+                  12 April 2021
+                </p>
+                <div className="absolute bottom-0 left-0 md:p-10 p-6">
+                  <h2 className="text-xl font-semibold 5 text-white">
+                    Importance Of Ayat ul Kursi
+                  </h2>
+                  <p className="text-base leading-4 text-white mt-2">
+                    The Quran is the living
+                  </p>
+                  <a
+                    href="https://blog.quranmajeed.com/importance-and-benefits-of-ayat-ul-kursi/"
+                    className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline"
+                  >
+                    <p className="pr-2 text-sm font-medium leading-none">
+                      Read More
+                    </p>
+                    <svg
+                      className="fill-stroke"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.75 12.5L10.25 8L5.75 3.5"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/5d016c1ba9f2e7000120c08c/1563304115766-MSX2KK1CN6P2X2KO2UWG/383555-PBYIW7-89.jpg?format=2500w"
+                alt="sitting place"
+                className="w-full sm:block hidden rounded-lg"
+              />
+            </div>
+            <div className="sm:flex items-center justify-between xl:gap-x-8 gap-x-6 md:mt-6 mt-4">
+              <div className="relative w-full">
+                <div>
+                  <p className="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
+                    September 11, 2023
+                  </p>
+                  <div className="absolute bottom-0 left-0 p-6">
+                    <h2 className="text-xl font-semibold 5 text-white">
+                      Dua For Breaking Fast
+                    </h2>
+                    <p className="text-base leading-4 text-white mt-2">
+                      Fasting is one of the
+                    </p>
+                    <a
+                      href="https://blog.quranmajeed.com/dua-for-breaking-fast/ "
+                      className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline"
+                    >
+                      <p className="pr-2 text-sm font-medium leading-none">
+                        Read More
+                      </p>
+                      <svg
+                        className="fill-stroke"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M5.75 12.5L10.25 8L5.75 3.5"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <img
+                  src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-1674678594.jpg?crop=0.6900191938579654xw:1xh;center,top&resize=980:*"
+                  className="w-full rounded-lg"
+                  alt="chair"
+                />
+              </div>
+              <div className="relative w-full sm:mt-0 mt-4">
+                <div>
+                  <p className="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
+                    March 11, 2021
+                  </p>
+                  <div className="absolute bottom-0 left-0 p-6">
+                    <h2 className="text-xl font-semibold 5 text-white">
+                      Rules of fasting
+                    </h2>
+                    <p className="text-base leading-4 text-white mt-2">
+                      During Ramadan,
+                    </p>
+                    <a
+                      href="https://www.goodhousekeeping.com/holidays/a32209842/what-is-ramadan/"
+                      className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline"
+                    >
+                      <p className="pr-2 text-sm font-medium leading-none">
+                        Read More
+                      </p>
+                      <svg
+                        className="fill-stroke"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M5.75 12.5L10.25 8L5.75 3.5"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <img
+                  src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-1674678992.jpg?crop=0.573xw:1.00xh;0.427xw,0&resize=980:*"
+                  className="w-full rounded-lg"
+                  alt="wall design"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blogs;
